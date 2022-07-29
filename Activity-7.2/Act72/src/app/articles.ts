4d84f38fbf5da1de423048a967888af86a4fddf4
Emna Ghariani <eghariani@dom.tti>
2022-07-29 18:16:29 +0100

    updates on comments

--- Activity-7.2/Act72/src/app/articles.ts
+++ Activity-7.2/Act72/src/app/articles.ts
@@ -4,5 +4,5 @@ export interface Article {
     content: string;
     date:Date;
     author:string   ;
-    comment:string    
+    comment:Array<any>  
 }
--- Activity-7.2/Act72/src/app/mock-articles.ts
+++ Activity-7.2/Act72/src/app/mock-articles.ts
@@ -8,7 +8,7 @@ export const ARTICLES: Article[] = [
             content: "Dans ce témoignage, Sara Luque, IT Talent attraction & Business Development Specialist chez Talan en Espagne, nous raconte son expérience à la Talan Talent Attraction and Business Development Academy située dans le Centre d'excellence de Malaga. ",
             date:new Date('07-11-2022'),
             author:'Sara Luque',
-            comment:"La Tunisie compte beaucoup de femmes ingénieures. « Lors de mon stage de fin d’études à chez Talan en Tunisie, les équipes étaient mixtes. "
+            comment:[{author:'hbib', content:"J'ai intégré l'informatique à l'université pour des raisons économiques, plus que tout. Le métier d'informaticien était considéré comme très lucratif à l'époque et obtenir ce diplôme était mon billet pour une vie plus stable financièrement.!"},{author:'ahmed', content:"La Tunisie compte beaucoup de femmes ingénieures. « Lors de mon stage de fin d’études à chez Talan en Tunisie, les équipes étaient mixtes. "}, {author:'emna', content:"C’est très bénéfique d’acquérir des connaissances, des idées, des expériences, mais c’est important aussi de savoir les transmettre"}]
         },
         {   
             id:2, 
@@ -16,7 +16,7 @@ export const ARTICLES: Article[] = [
             content: "En mai 2022, Talan est devenu membre du Forum National de la Facturation Électronique et des marchés publiques (FNFE - MPE). À partir du mois de juillet 2024, toutes les entreprises françaises auront l'obligation d'être en mesure de réceptionner les factures au format électronique, selon des contraintes techniques spécifiées par l'État. De manière progressive, toutes les entreprises françaises devront également émettre des factures électroniques.",
             date:new Date('07-08-2022'),
             author:'Amaury Languillat',
-            comment:"J'ai intégré l'informatique à l'université pour des raisons économiques, plus que tout. Le métier d'informaticien était considéré comme très lucratif à l'époque et obtenir ce diplôme était mon billet pour une vie plus stable financièrement.!"
+            comment:[ {author:'emna', content:"C’est cet esprit de soutien et de partage qui m’a aidée à continuer ce que je fais et m’a incitée à y prendre part à mon tour"} ]
         },
         {   
             id:3,
@@ -24,7 +24,8 @@ export const ARTICLES: Article[] = [
             content: "Les principes de l’immersif portés par le métavers, ne se limitent pas au full-virtuel, de même que le digital ne se réduit pas au full digital. Il s’agit plutôt d’une tendance lourde qui consiste à mêler à différents degrés, réalité et virtuel, pour venir considérablement ouvrir l’éventail des possibilités dont les marques disposent pour enrichir les expériences qu’elles proposent à leurs clients. ",
             date:new Date('02-08-2022'),
             author:' Brigitte Gouarin',
-            comment:"J'ai toujours aimé les chiffres, l'analyse de données. Comprendre et résoudre des problèmes. Alors quand il a fallu choisir mes études, je me suis orientée vers les stastiques."
+            comment:[{author:'ahmed', content:"J'ai toujours aimé les chiffres, l'analyse de données. Comprendre et résoudre des problèmes. Alors quand il a fallu choisir mes études, je me suis orientée vers les stastiques."}
+        , {author:'emna',content:"Article bien rédigé"}]
         },
         {   
             id:4,
@@ -32,7 +33,8 @@ export const ARTICLES: Article[] = [
             content: "Pouvoir expliquer et comprendre des systèmes automatiques de prises de décision sont des composantes essentielles de la confiance dans de tels outils. L’Intelligence Artificielle, utilisée dans de tels modèles, doit ainsi être en mesure de donner des clés sur le cheminement de son apprentissage ou du raisonnement mis en œuvre. ",
             date:new Date('07-07-2022'),
             author:'Cécile Lafon',
-            comment:"Ce parcours m'a ouvert les portes du métier de Data Scientist que j'exerce chez Talan depuis décembre dernier. C'est un métier passionant"
+            comment:[{author:'ahmed', content:"Ce parcours m'a ouvert les portes du métier de Data Scientist que j'exerce chez Talan depuis décembre dernier."},{author:'emna',content:"Article bien rédigé"}, {author:'rania', content:" C'est un métier passionant"}]
+  
         },
         {   
             id:5,
@@ -40,15 +42,15 @@ export const ARTICLES: Article[] = [
             content: " La startup Skeewai, co-fondée notamment par le Dr Mehdi Roudesli, médecin spécialisé en médecine sportive et Nicolas Ducrocq, kinésithérapeute, a mis au point Recovup, une solution destinée à prévenir les troubles muscolo-squelettiques (TMS) et à faciliter l’auto-rééeducation",
             date:new Date('07-06-2022'),
             author:'Marjorie Philippon',
-            comment:"uand on s'intéresse au secteur technologique, il y a deux softskills qui prennent le dessus"
-        },
+            comment:[{author:'ahmed', content:"La Tunisie compte beaucoup de femmes ingénieures. « Lors de mon stage de fin d’études à chez Talan en Tunisie, les équipes étaient mixtes. "},  {author:'emna', content:"C’est très bénéfique d’acquérir des connaissances, des idées, des expériences, mais c’est important aussi de savoir les transmettre"}, {author:'hbib', content:"C’est très bénéfique d’acquérir des connaissances, des idées, des expériences, mais c’est important aussi de savoir les transmettre"},{author:'kenza', content:"C’est très bénéfique d’acquérir des connaissances, des idées, des expériences, mais c’est important aussi de savoir les transmettre"}]},
         {
             id:5,
             title: " STRATÉGIE RSE : OUTILLEZ-VOUS POUR AVOIR UN IMPACT RÉEL !",
             content: " Un webinar en partenariat avec Board a eu lieu le 16 juin. Le replay est disponible !",
             date:new Date('07-04-2022'),
            author:'Stéphanie Butorovic',
-            comment:"Le secteur des nouvelles technologies est vaste et en constante évolution."
+              comment:[{author:'ahmed', content:"J'ai toujours aimé les chiffres, l'analyse de données. Comprendre et résoudre des problèmes. Alors quand il a fallu choisir mes études, je me suis orientée vers les stastiques."}
+        , {author:'emna',content:"Article bien rédigé"}]
         },
         {  
             id:6,
@@ -56,54 +58,7 @@ export const ARTICLES: Article[] = [
             content: "Le métavers vient percuter l’ensemble des dimensions de l’activité et du fonctionnement des entreprises : il se révèle notamment un formidable terrain de jeu pour le marketing. ",
             date:new Date('01-07-2022'),
             author:' Brigitte Gouarin',
-            comment:" C’est cet esprit de soutien et de partage qui m’a aidée à continuer ce que je fais et m’a incitée à y prendre part à mon tour "
+            comment:[{author:'ahmed', content:"La Tunisie compte beaucoup de femmes ingénieures. « Lors de mon stage de fin d’études à chez Talan en Tunisie, les équipes étaient mixtes. "},]
         },
-        // {  
-        //     id:7,
-        //     title: "ECOCODE CHALLENGE, TALAN RELÈVE LE DÉFI ! ",
-        //     content: " Le 2 et 3 Juin dernier, à l’initiative du Groupe Crédit Agricole, a eu lieu un hackathon d’un nouveau type  L’ecoCode Challenge .",
-        //     date:new Date('30-06-2022'),
-        //    author:'Amaury Languillat',
-        //     comment:"Et il reste encore beaucoup de terrain de jeux innovants à explorer sur lesquels la technologie aurait un rôle majeur à jouer au profit d'une société plus inclusive et responsable."
-        // },
-        // { 
-        //     id:9,
-        //     title: "RENCONTREZ BHAGYASHREE \"SHREE\" CHOUHAN ",
-        //     content: "Dans ce témoignage, Bhagyashree \"Shree\" Chouhan nous raconte son expérience en tant que \"Business Analyst\" chez Talan au Canada. ",
-        //     date:new Date('29-06-2022'),
-        //    author:'Stéphanie Butorovic',
-        //     comment:"J'ai aussi la nette impression que de plus en plus de femmes occupent des postes de direction."
-        // },
-        // {  
-        //     id:10,
-        //     title: " INTERVIEW DE JONATHAN SAMARAS, DIRECTEUR GÉNÉRAL BELUX, PARTENAIRE DU GROUPE TALAN",
-        //     content: "Le Groupe Talan a annoncé le lancement de nouvelles opération en Belgique en Janvier 2022 Six mois plus tard, nous nous entretenons avec Jonathan Samaras qui a pris en charge ce projet d'expansion en Belgique. ",
-        //     date:new Date('27-06-2022'),
-        //    author:'Stéphanie Butorovic',
-        //     comment:"C’est très bénéfique d’acquérir des connaissances, des idées, des expériences, mais c’est important aussi de savoir les transmettre"
-        // },
-        // {  
-        //     id:11,
-        //     title: " RENCONTRE AVEC GUILLAUME CAMBEFORT, SALES CONSULTANT CHEZ TALAN EN ESPAGNE",
-        //     content: "Guillaume Cambefort a rejoint Talant en janvier 2022. Dans ce témoignage, Guillaume nous raconte son expérience au sein de la Talan Talent Attraction and Business Development Academy située dans le centre d'excellence de Malaga. ",
-        //     date:new Date('24-06-2022'),
-        //    author:'Cécile Lafon',
-        //     comment:" J’ai vu beaucoup personnes qui hésitaient et ne savaient pas comment arriver là où j’en suis"
-        // },
-        // {  
-        //     id:12,
-        //     title: "CÉCILE PETIT, GESTIONNAIRE DE PROJET CHEZ CREATECH : « JE N’AI PAS ÉTÉ DÉTERMINÉE PAR MES DIPLÔMES MAIS PAR MES COMPÉTENCES ET MA CAPACITÉ À APPRENDRE » ",
-        //     content: "Basée à Montréal, Cécile Petit vient de fêter ses 4 ans chez Createch et son 20e anniversaire sur le territoire canadien. Après avoir vécu un projet d’implantation ERP côté client, cette Franco-Canadienne a su faire sa place dans un univers plutôt masculin en tant que conseillère fonctionnelle au sein de l’équipe Microsoft chez Createch puis gestionnaire de projets. Au fil des années, elle a développé ses compétences techniques tout en cultivant son approche personnelle d’accompagnement des clients tournée vers l’humain. ",
-        //     date:new Date('23-06-2022'),
-        //    author:'Amaury Languillat',
-        //     comment:"La Tunisie compte beaucoup de femmes ingénieures. « Lors de mon stage de fin d’études à chez Talan en Tunisie, les équipes étaient mixtes. "
-        // },
-        // {  
-        //     id:13,
-        //     title: " LE MÉTAVERS VU DES ENTREPRISES : 3 TEMPS POUR L’APPRÉHENDER",
-        //     content: "Depuis quelques mois, le Métavers est devenu le buzzword de tous les médias et des milieux économiques. Face à ce brouhaha dans lequel il est bien difficile de se retrouver, les dirigeants d’entreprises se posent de nombreuses questions ",
-        //     date:new Date('22-06-2022'),
-        //     author:'Marjorie Philippon',
-        //     comment:" C’est cet esprit de soutien et de partage qui m’a aidée à continuer ce que je fais et m’a incitée à y prendre part à mon tour "
-        // }
+    
     ]
